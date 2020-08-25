db7557966b6ceea06887aaf5641999ba9e2ab425
freepluslq <liuqian227362@gmail.com>
2020-08-25 12:21:37 +0800

    Update PeakCalling.sh

--- Code/PeakCalling.sh
+++ Code/PeakCalling.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+#a sample of peakcalling for all normal samples
 cd ./ATAC-pipe-master
-python atac-pipe.py --PeakCalling --bed ./BedFiles -r hg19 -o ./outputFiles --group ./SourceData/Groups.txt --p1 3 --q1 5 --pipeup 10 -u
+python atac-pipe.py --PeakCalling --bed ./BedFiles -r hg19 -o ./outputFiles --group ./SourceData/NormalSamples_Groups.txt --p1 3 --q1 5 --pipeup 10 -u
